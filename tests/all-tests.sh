8f78b7f800e9b32369cfe495dd9d45a347888471
MaxymVlasov <m.vlasov@post.com>
2019-02-12 13:16:18 +0200

    Add interractive warning for commit >=1000 changes
    Issue: https://github.com/pdffiller/styleguide-hooks/issues/21

--- tests/all-tests.sh
+++ tests/all-tests.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+# Set settings for tests
+SETTINGS="../hooks/_user_defined_settings.sh"
+if [ -f ${SETTINGS} ]; then
+    mv $SETTINGS "${SETTINGS}.save"
+fi
+
+cat << EOF > ${SETTINGS}
+### For tests ###
+INTERACTIVE_MODE="disabled"
+FORСE_WARNING_TO_ERROR="enabled"
+#################
+EOF
+
+# Run tests
 for file in *.bats; do
     echo "Run tests for $file" | cut -d'.' -f1
     bats $file
     echo ''
 done
+
+# Restore user settings
+rm $SETTINGS
+
+if [ -f "${SETTINGS}.save" ]; then
+    mv  "${SETTINGS}.save" $SETTINGS
+fi
